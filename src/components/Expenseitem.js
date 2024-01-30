231969fecd5258676d211c20051c9b84a73825e0
singh830 <singhsuhani830>
2024-01-30 16:18:42 +0530

    updated

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -4,6 +4,7 @@ function Expenseitem() {
     const expenseDate = new Date(2024,2,2029); 
     const expenseTitle = 'Car Insurance';
     const expenseAmount = 294.70;
+    const LocationOfExpenditure = 'Grocery Store';
     return (
         <div className="expense-item">
         <div>{expenseDate.toISOString}</div>;
@@ -11,6 +12,7 @@ function Expenseitem() {
                 <h2>{expenseTitle}</h2>
                 <div className="expense-item__price">${expenseAmount}</div>
             </div>
+        <p>Location: {LocationOfExpenditure}</p>    
     </div>
     );
     
