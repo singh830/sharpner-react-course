c952a949d19c5a954b34876c79daaa5194bb1713
singh830 <singhsuhani830>
2024-01-30 15:53:42 +0530

    updated

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,9 +1,16 @@
-function Expenseitem(){
-    return <><h2>Expense Item </h2><ul>
-        <li>Food Rs 10</li>
-        <li>Petrol Rs 100</li>
-        <li>Movies Rs 200</li>
-    </ul></>
+import './Expenseitem.css';
+
+function Expenseitem() {
+    return (
+        <div className="expense-item">
+        <div>Jan 29 2023</div>;
+        <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$294.70</div>
+            </div>
+    </div>
+    );
+    
 }
 
 export default Expenseitem;
