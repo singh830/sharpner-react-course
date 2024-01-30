e833418e45ed65808e14e279294b5c2e80bd2419
singh830 <singhsuhani830>
2024-01-30 13:34:18 +0530

    react first code

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,9 @@
+function Expenseitem(){
+    return <><h2>Expense Item </h2><ul>
+        <li>Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 200</li>
+    </ul></>
+}
+
+export default Expenseitem;
