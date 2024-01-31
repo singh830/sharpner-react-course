2a75e9071e462a5edcfb2996f9c294e93fbc0664
singh830 <singhsuhani830>
2024-01-31 22:20:18 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenseitem  from './components/Expenseitem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -26,14 +27,11 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
- 
+
   return (
     <div>
       <h2>Let's get started!</h2>
-          <Expenseitem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-          <Expenseitem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-          <Expenseitem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-          <Expenseitem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,15 +1,17 @@
 import ExpenseDate from './ExpenseDate';
+import card from './card';
 import './Expenseitem.css';
+
 
 function Expenseitem(props) {
     return (
-        <div className="expense-item">
-        <ExpenseDate  date={props.date}/>   
-        <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
+        <card className="expense-item">
+            <ExpenseDate  date={props.date}/>   
+            <div className="expense-item__description">
+              <h2>{props.title}</h2>
+              <div className="expense-item__price">${props.amount}</div>
             </div>   
-    </div>
+        </card>
     );
     
 }
