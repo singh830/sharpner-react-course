ce68efa4eedafbbdfaabdc4122cc8a1ed1104652
singh830 <singhsuhani830>
2024-02-01 20:14:20 +0530

    update

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -5,13 +5,21 @@ import './Expenseitem.css';
 
 
 const Expenseitem = (props) => {
+    const clickHandler=() =>{
+        console.log('Clicked!!!');
+    };
+    const deleteHandler=()=>{
+        props.onDelete('props.id');
+    };
     return (
         <card className="expense-item">
             <ExpenseDate  date={props.date}/>   
             <div className="expense-item__description">
               <h2>{props.title}</h2>
               <div className="expense-item__price">${props.amount}</div>
-            </div>   
+            </div> 
+            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={deleteHandler}>Delete Expense</button>
         </card>
     );
     
